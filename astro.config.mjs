7dbb0111b50f8830d06edaeee687e486e259d788
Justin Smith <justinasmith662@gmail.com>
2025-01-12 18:43:42 -0600

    Integrate sitemap and enhance SEO: Update astro.config.mjs to include @astrojs/sitemap and configure robots.txt settings for improved search engine optimization. Add sitemap link in Layout.astro and correct email address in contact.astro. Update package.json and pnpm-lock.yaml to include sitemap dependency. These changes aim to enhance site visibility and user navigation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,11 @@ import icon from "astro-icon";
 
 import robots from "astro-robots";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://stateofmindpsychiatric.com",
   integrations: [
     react(),
     tailwind({
@@ -19,7 +22,40 @@ export default defineConfig({
     }),
     mdx(),
     icon(),
-    robots(),
+    robots({
+      host: "https://stateofmindpsychiatric.com",
+      sitemap: [
+        "https://stateofmindpsychiatric.com/sitemap-index.xml",
+        "https://www.stateofmindpsychiatric.com/sitemap-index.xml",
+      ],
+      policy: [
+        {
+          userAgent: [
+            "Applebot",
+            "Googlebot",
+            "bingbot",
+            "Yandex",
+            "Yeti",
+            "Baiduspider",
+            "360Spider",
+            "*",
+          ],
+          allow: ["/"],
+          disallow: ["/admin", "/login"],
+          crawlDelay: 5,
+          cleanParam: ["sid /", "s /forum/showthread"],
+        },
+        {
+          userAgent: "BLEXBot",
+          disallow: ["/assets", "/uploades/1989-08-21/*jpg$"],
+        },
+      ],
+    }),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
   ],
 
   adapter: vercel({
